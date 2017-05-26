9d83ea22aed680508c38857afb89b3771f55b0cc
Jay Park <flexiblethink@gmail.com>
2017-05-26 11:07:08 +0900

    v1

---
+++ app/src/main/java/com/androidimageslider/MainActivity.java
@@ -0,0 +1,22 @@
+package com.androidimageslider;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    int images[] = {R.drawable.s1, R.drawable.s2, R.drawable.s3, R.drawable.s4};
+    MyCustomPagerAdapter myCustomPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        myCustomPagerAdapter = new MyCustomPagerAdapter(MainActivity.this, images);
+        viewPager.setAdapter(myCustomPagerAdapter);
+    }
+}
